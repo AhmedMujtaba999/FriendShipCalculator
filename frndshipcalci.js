9413fe5fc64e9f27378325184f619d43f1a5c42f
AhmedMujtaba999 <116835811+AhmedMujtaba999@users.noreply.github.com>
2023-12-11 15:39:08 +0530

    frndshipcalci.js

---
+++ frndshipcalci.js
@@ -0,0 +1,5 @@
+// This calculator tells you the random percentage of your friendship
+var yourname=prompt("please enter your name");
+var frndsname=prompt("please enter your friends name");
+var n=Math.random();
+alert("Your friendship calculation is: "+Math.floor(n*100+1)+"%");
