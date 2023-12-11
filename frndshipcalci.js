394e318d9f0c8f5f5534f3b033dd33e43859228a
AhmedMujtaba999 <116835811+AhmedMujtaba999@users.noreply.github.com>
2023-12-11 15:49:42 +0530

    random number generator and control flow stmts

--- frndshipcalci.js
+++ frndshipcalci.js
@@ -3,4 +3,10 @@ var yourname=prompt("please enter your name");
 var frndsname=prompt("please enter your friends name");
 var n=Math.random();
 n=Math.floor(n*100)+1;
+if(n>70)
+{
+  alert("your friendship score is: "+ n +"%"+" you both are best freinds");
+}
+else{
 alert("Your friendship calculation is: "+ n +"%");
+}
